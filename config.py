4001d601fb128b8879d46b9184dee5587ad6e6c0
jayden7475 <67633913+jayden7475@users.noreply.github.com>
2023-04-11 15:12:07 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.cvkqzfhdsqqc.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "leemingswin-employee"
+customregion = "us-east-1"
